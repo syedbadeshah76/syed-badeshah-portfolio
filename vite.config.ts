d84d3f27cf40c6195685be0cfd4e904ca7d7d04f
syedbadeshah76 <151610853+syedbadeshah76@users.noreply.github.com>
2025-09-17 23:21:00 +0530

    initial commitsss

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/",
+  base:"/syed-badeshah-portfolio",
   server: {
     host: "::",
     port: 8080,
